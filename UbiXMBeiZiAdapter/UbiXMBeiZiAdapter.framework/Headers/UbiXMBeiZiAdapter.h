c92447fdb41a6ec49a798ff1f8fcc481b65fd1d8
guoqiang <guoqiang.zhu@ubixai.com>
2024-07-05 11:51:13 +0800

    add BeiZiAdapter 4.90.3.1.0

---
+++ UbiXMBeiZiAdapter/UbiXMBeiZiAdapter.framework/Headers/UbiXMBeiZiAdapter.h
@@ -0,0 +1,23 @@
+//
+//  UbiXMBeiZiAdapter.h
+//  UbiXMBeiZiAdapter
+//
+//  Created by guoqiang.zhu on 2024/6/19.
+//  Copyright © 2024 UBiX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface UbiXMBeiZiAdapter : NSObject
+//
+//@end
+
+//! Project version number for UbiXMBeiZiAdapter.
+FOUNDATION_EXPORT double UbiXMBeiZiAdapterVersionNumber;
+
+//! Project version string for UbiXMBeiZiAdapter.
+FOUNDATION_EXPORT const unsigned char UbiXMBeiZiAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+
